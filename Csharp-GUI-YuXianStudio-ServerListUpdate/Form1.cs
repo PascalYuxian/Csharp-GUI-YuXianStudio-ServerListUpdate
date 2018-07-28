ee5916d7a3f9cf25e94f18210b6628d95e2cb6dc
PascalYuxian <343685355@qq.com>
2018-07-28 20:59:00 +0800

    添加项目文件。

---
+++ Csharp-GUI-YuXianStudio-ServerListUpdate/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Csharp_GUI_YuXianStudio_ServerListUpdate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Csharp-GUI-YuXianStudio-ServerListUpdate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Csharp_GUI_YuXianStudio_ServerListUpdate
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
